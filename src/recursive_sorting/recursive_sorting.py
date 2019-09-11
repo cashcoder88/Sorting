809766ef7a006bc5b7af36497697d1bc2cc7d088
cashcoder88 <cashglobe@gmail.com>
2019-09-11 15:02:17 -0600

    re-implementation of merge function, based on lecture

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,9 +1,34 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
+# we need to sort arrA and arrB
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+    # Start merging my single lists of one element together into large, sorted sets
+    # Repeat 3 till the entire data set is reassembled
+    b = 0 #current arrA index
+    a = 0 #current arrB index
+# for each index in the merged arr: elements
+    for i in range(num_elements):
+        # find the smallest first-item between arrA and arrB
+        #add that to elements at its given index
+        # increment the a/b counter to move through indexes
+        if a >= len(arrA):
+            #first case, A is empty, b in not
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrA):
+            #second case, A is not empty, b is
+            merged_arr[i] = arra[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            #third case, A has the smaller element
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            #fourth case, B has the smaller element
+            merged_arr[i] = arrB[b]
+            b += 1
+    # return this new array
     return merged_arr
 
 
